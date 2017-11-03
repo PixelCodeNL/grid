8ea2ed484b55d2acfbdd70babc97e6201dd3d831
Thijs Kaspers <thijs@24creative.nl>
2017-11-03 15:46:33 +0100

    Added first setup

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+/* Set src / distribution paths */
+var src = 'src';
+var paths = {
+    sassGlobal: src + '/scss/**/*.scss',
+    sassCompile: src + '/scss/*.scss'
+};
+
+/* Sass */
+gulp.task('sass', function () {
+    gulp.src(paths.sassCompile)
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+});
+
+/* Watch these files for changes and run the task on update */
+gulp.task('watch', function () {
+    gulp.watch(paths.sassGlobal, ['sass']);
+});
+
+/* Default task(s) when gulp is called without arguments */
+gulp.task('default', ['watch']);
+
+/* Build task */
+gulp.task('build', ['sass']);
